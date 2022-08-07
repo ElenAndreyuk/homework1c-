118552e5e199ee4bf6cce6b36012ab4a53152216
elena <femtochka@yahoo.com>
2022-08-07 16:01:09 +0300

    первая задача

---
+++ task001/Program.cs
@@ -0,0 +1,13 @@
+﻿//программа выбирает максимальное число
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine("Максимальное число "+ a);
+}
+if (b > a )
+{
+    Console.WriteLine("Максимальное число "+ b);
+}
