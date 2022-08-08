3b86f281b79fdc2c7d78d34c8f0bfc5ca72b8ecc
elena <femtochka@yahoo.com>
2022-08-08 15:39:10 +0300

    четвертая задача

---
+++ task004/Program.cs
@@ -0,0 +1,9 @@
+﻿// Четные числа от 1 до N
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while ( i <= N )
+{
+    Console.WriteLine( i );
+    i = i + 2;
+}
