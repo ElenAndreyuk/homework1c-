c9dd0e80db69693b8cc0c8cc056f9e07757b4562
elena <femtochka@yahoo.com>
2022-08-08 15:29:59 +0300

    третья задача

---
+++ task003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Четное или нечетное
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 2;
+if ( b == 0)
+{
+    Console.WriteLine("Четное");
+}
+else 
+{
+    Console.WriteLine("нечетное");
+}
